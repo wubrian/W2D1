c0a0c097700cf3817aaf49076d89f95145f59031
Brian Wu <wubrian1234@hotmail.com>
2018-12-03 22:10:36 +0000

    add get html

---
+++ getHTML.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+function getHTML (options, printHTML) {
+
+  /* Add your code here */
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    var allData = "";
+
+    response.on('data', function (data) {
+      allData += data;
+    });
+
+    response.on('end', function (data) {
+      printHTML(allData);
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
